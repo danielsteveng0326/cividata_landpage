4bf20d473cc77d1e2e6d174e516ba87451c460ec
danielsteveng0326 <danielsteven0326@gmail.com>
2025-06-07 02:12:06 -0500

    fix front y back 4.1

--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="cividata_company_landing_page",
+    frontend_port=3000,  # Cambia esto al puerto que quieras
+    backend_port=8000,   # Cambia esto también si es necesario
 )
